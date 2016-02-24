be85daaf42bb0e62a10162ed8207a05668a214e3
Amit Karnik <amit.karnik@edgenetworks.in>
2016-02-24 08:45:22 +0530

    Apply fn separately to each element in list

--- examples.py
+++ examples.py
@@ -16,3 +16,18 @@ print map(lambda t: (float(9)/5)*t + 32, temp)
 a = b = range(10)
 print map(lambda x, y: x+y, a, b)
 
+# applying fn separately to each element in lists
+a1, b1, c1, d1 = range(10), range(20, 30), range(30, 40), range(40, 50)
+temp = map(lambda *args: [i**2 for i in args], a1, b1, c1, d1)
+list_name = {}
+for i in range(len(temp[0])):
+    list_name["list_"+str(i)] = []
+
+for i in temp:
+    m = 0
+    for j in i:
+        list_name["list_" +str(m)].append(j)
+        m += 1
+
+for m in list_name:
+    print list_name[m]
